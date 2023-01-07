17dd3ff806884aa4af9f56f33725fd44faffa55c
Ingo Albrecht <commit@promovicz.org>
2023-01-07 02:37:46 +0100

    Reorder includes

--- multilink.c
+++ multilink.c
@@ -64,16 +64,16 @@
  * renames everything else according to the basename argument - djw.
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include <sys/param.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <ctype.h>
 #include <arpa/inet.h>
 
 #include <bfd.h>
